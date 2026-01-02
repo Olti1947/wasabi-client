b34faafa1dec62ab6c03b7ec596175dfdbf854f1
Olti1947 <oltibajrami123@gmail.com>
2026-01-02 13:07:19 +0100

    Added icons to tab layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,6 @@
 import { selectIsAuthenticated } from "@/src/features/auth/authSelectors";
+import { colors } from "@/src/theme/colors";
+import { Ionicons } from '@expo/vector-icons';
 import { Redirect, Tabs } from "expo-router";
 import { useSelector } from "react-redux";
 
@@ -10,8 +12,28 @@ if (!isAuthenticated){
 }
 
     return <Tabs
-    screenOptions={{headerShown: false}}
+    screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: '#8e8e93',
+
+    }}
     >
-        <Tabs.Screen name = "index" options={{title: "Home"}} />
+        <Tabs.Screen 
+        name = "index" 
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }} />
+        <Tabs.Screen 
+        name = "settings" 
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }} />
     </Tabs>;
 }
